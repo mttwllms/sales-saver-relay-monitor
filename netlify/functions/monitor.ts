c154870a5a405fe1667a6c7b19047482a36fa897
mttwllms <6372961+mttwllms@users.noreply.github.com>
2025-02-13 11:30:40 -0800

    added email notifications

--- netlify/functions/monitor.ts
+++ netlify/functions/monitor.ts
@@ -1,46 +1,48 @@
 import { schedule } from '@netlify/functions'
 import axios from "axios";
-// import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 // Get environment variables
-// dotenv.config();
-// const { TIMEOUT_LIMIT, MAILER_TO, MAILER_FROM, MAILER_PASS, ENDPOINT } = process.env;
+dotenv.config();
+const { CRON, MAILER_TO, MAILER_FROM, MAILER_PASS, ENDPOINT, TIMEOUT_LIMIT: timeoutLimit } = process.env;
+const TIMEOUT_LIMIT = Number(timeoutLimit);
 
-export const handler = schedule('* * * * *', async (event: any) => {
+export const handler = schedule(`${CRON}`, async (event: any) => {
   const requestTimestamp = new Date();
+
+  async function sendEmail(subject, body) {
+    let transporter = nodemailer.createTransport({
+      service: "gmail", // Use your email provider
+      auth: {
+        user: MAILER_FROM,
+        pass: MAILER_PASS,
+      },
+    });
+  
+    let mailOptions = {
+      from: MAILER_FROM,
+      to: MAILER_TO,
+      subject: subject,
+      text: body,
+    };
+  
+    await transporter.sendMail(mailOptions);
+  }
+  
   try {
-    const response = await axios.get('https://rio-dev.sportsbasement.com/ss', { timeout: 5000 });
-    const responseTimestamp = requestTimestamp.getTime() - new Date().getTime();
+    const response = await axios.get(`${ENDPOINT}`, { timeout: TIMEOUT_LIMIT });
+    const responseTimestamp = new Date().getTime() - requestTimestamp.getTime();
     const body = `Sales Saver Relay status: ${response.status} at ${requestTimestamp} in ${responseTimestamp}ms`;
+    await sendEmail("Sales Saver Relay is working", body);
     console.log(body);
     return { statusCode: response.status, body: body };
   } catch (error) {
-    // const subject = 'Sales Saver relay not responding';
-    // const body = `Sales Saver relay is not responding at ${timestamp}:\n\n${error.status}\n${error.code}\n${error.message}`;
-    // await sendEmail(subject, body);
-    const responseTimestamp = requestTimestamp.getTime() - new Date().getTime();
+    const responseTimestamp = new Date().getTime() - requestTimestamp.getTime();
+    const subject = 'Sales Saver Relay not responding';
     const body = `Sales Saver Relay status: ${error.status} at ${requestTimestamp} in ${responseTimestamp}ms`;
+    await sendEmail(subject, body);
     console.log(body);
     return { statusCode: error.status, body: body };
   }
 });
-
-
-
-// TEST FUNCTION
-// export const handler = async (event: any, context: any) => {
-//   const requestTimestamp = new Date();
-//   try {
-//     const response = await axios.get('https://rio-dev.sportsbasement.com/ss', { timeout: 5000 });
-//     const responseTimestamp = requestTimestamp.getTime() - new Date().getTime();
-//     const body = `Sales Saver Relay status: ${response.status} at ${requestTimestamp} in ${responseTimestamp}ms`;
-//     return { statusCode: response.status, body: body };
-//   } catch (error) {
-//     // const subject = 'Sales Saver relay not responding';
-//     // const body = `Sales Saver relay is not responding at ${timestamp}:\n\n${error.status}\n${error.code}\n${error.message}`;
-//     // await sendEmail(subject, body);
-//     const responseTimestamp = requestTimestamp.getTime() - new Date().getTime();
-//     const body = `Sales Saver Relay status: ${error.status} at ${requestTimestamp} in ${responseTimestamp}ms`;
-//     return { statusCode: error.status, body: error.message };
-//   }
-// };
