aa46084b03ef67756d5316005c48ef5e7e2404d6
mttwllms <6372961+mttwllms@users.noreply.github.com>
2025-02-12 16:53:45 -0800

    pause test scheduled function

--- netlify/functions/monitor.ts
+++ netlify/functions/monitor.ts
@@ -1,13 +1,13 @@
-import { schedule } from '@netlify/functions'
+// import { schedule } from '@netlify/functions'
 
-export const handler = schedule('* * * * *', async (event: any) => {
-  const timestamp = new Date().toISOString();
-  console.log(`Hello from monitor @ ${timestamp}`);
-  return {
-    statusCode: 200,
-    body: `Hello from monitor @ ${timestamp}`
-  }
-});
+// export const handler = schedule('* * * * *', async (event: any) => {
+//   const timestamp = new Date().toISOString();
+//   console.log(`Hello from monitor @ ${timestamp}`);
+//   return {
+//     statusCode: 200,
+//     body: `Hello from monitor @ ${timestamp}`
+//   }
+// });
 
 // TEST FUNCTION
 // export const handler = async (event: any, context: any) => {
